andreasgal/B2G

commit 3d12fe0dbca25f44d685dd058c68d528cd8e05d7

Add support for B2G widget backend

Makefile
@@ -25,6 +25,7 @@ endef
 
 ANDROID_SDK_PLATFORM ?= android-13
 GECKO_CONFIGURE_ARGS ?=
+WIDGET_BACKEND ?= android
 
 # Developers can use this to define convenience rules and set global variabls
 # XXX for now, this is where to put ANDROID_SDK and ANDROID_NDK macros
@@ -33,12 +34,14 @@ GECKO_CONFIGURE_ARGS ?=
 .PHONY: build
 build: gonk gecko
 
+ifeq (android,$(WIDGET_BACKEND))
 ifndef ANDROID_SDK
 $(error Sorry, you need to set ANDROID_SDK in your environment to point at the top-level of the SDK install.  For now.)
 endif
 
 ifndef ANDROID_NDK
 $(error Sorry, you need to set ANDROID_NDK in your environment to point at the top-level of the NDK install.  For now.)
+endif
 endif
 
 .PHONY: gecko
@@ -55,7 +58,7 @@ gecko:
 	make -C gecko/objdir-prof-android package
 
 .PHONY: gonk
-gonk: geckoapk-hack gaia-hack
+gonk: gecko-$(WIDGET_BACKEND)-hack gaia-hack
 	@$(call GONK_CMD,make $(MAKE_FLAGS) $(GONK_MAKE_FLAGS))
 
 .PHONY: kernel
@@ -80,7 +83,7 @@ clean-kernel:
 	@PATH="$$PATH:$(abspath $(TOOLCHAIN_PATH))" make -C $(KERNEL_PATH) ARCH=arm CROSS_COMPILE=arm-eabi- clean
 
 .PHONY: config-galaxy-s2
-config-galaxy-s2: config-gecko-gonk
+config-galaxy-s2: config-gecko-$(WIDGET_BACKEND)
 	@echo "KERNEL = galaxy-s2" > .config.mk && \
 	echo "GONK = galaxys2" >> .config.mk && \
 	cp -p config/kernel-galaxy-s2 boot/kernel-android-galaxy-s2/.config && \
@@ -89,9 +92,13 @@ config-galaxy-s2: config-gecko-gonk
 	./extract-files.sh && \
 	echo OK
 
-.PHONY: config-gecko-gonk
-config-gecko-gonk:
-	@cp -p config/gecko-prof-gonk gecko/.mozconfig
+.PHONY: config-gecko-android
+config-gecko-android:
+	@cp -p config/gecko-prof-android gecko/mozconfig
+
+.PHONY: config-gecko-b2g
+config-gecko-b2g:
+	@cp -p config/gecko-prof-b2g gecko/mozconfig
 
 define INSTALL_NEXUS_S_BLOB # $(call INSTALL_BLOB,vendor,id)
 	wget https://dl.google.com/dl/android/aosp/$(1)-crespo4g-grj90-$(2).tgz && \
@@ -102,7 +109,7 @@ endef
 
 .PHONY: config-nexuss4g
 # XXX Hard-coded for nexuss4g target
-config-nexuss4g: config-gecko-gonk
+config-nexuss4g: config-gecko-android
 	@echo "KERNEL = samsung" > .config.mk && \
 	echo "GONK = crespo4g" >> .config.mk && \
 	cp -p config/kernel-nexuss4g boot/kernel-android-samsung/.config && \
@@ -166,12 +173,20 @@ APP_OUT_DIR := $(OUT_DIR)/app
 $(APP_OUT_DIR):
 	mkdir -p $(APP_OUT_DIR)
 
-.PHONY: geckoapk-hack
-geckoapk-hack: gecko
+.PHONY: gecko-android-hack
+gecko-android-hack: gecko
 	mkdir -p $(APP_OUT_DIR)
 	cp -p gecko/objdir-prof-android/dist/b2g-*.apk $(APP_OUT_DIR)/B2G.apk
 	unzip -jo gecko/objdir-prof-android/dist/b2g-*.apk lib/armeabi-v7a/libmozutils.so -d $(OUT_DIR)/lib
 	find glue/gonk/out -iname "*.img" | xargs rm -f
+
+.PHONY: gecko-b2g-hack
+gecko-b2g-hack: gecko
+	rm -rf $(OUT_DIR)/b2g
+	( cd $(OUT_DIR) && \
+	  tar xvfz $(PWD)/gecko/objdir-prof-android/dist/b2g-*.tar.gz )
+	find glue/gonk/out -iname "*.img" | xargs rm -f
+
 
 .PHONY: gaia-hack
 gaia-hack: gaia

INSTALL
@@ -5,17 +5,6 @@ $B2G.
  Dependencies
 --------------
 
-* Sadly, B2G currently depends on not only the android source, but
-  also the android SDK and NDK.  These dependencies will be removed
-  eventually, but for now you need to install both.  Follow the
-  instructions at
-
-    http://developer.android.com/sdk/installing.html
-    http://developer.android.com/sdk/ndk/index.html
-
-  From here on, this document assumes you installed the SDK to the
-  path $ANDROID_SDK and the NDK to the path $ANDROID_NDK.
-
 * You need to have adb set up to allow your machine to talk to your
   phone.  Follow the instructions at
 
@@ -47,6 +36,20 @@ $B2G.
       flex gperf lib32z1-dev lib32ncurses5-dev lib32ncursesw5-dev \
       libidl-dev lib32gomp1 autoconf2.13 ccache libx11-dev \
       lib32readline-gplv2-dev
+
+ Additional dependencies when building android backend
+-------------------------------------------------------
+
+* Sadly, B2G currently depends on not only the android source, but
+  also the android SDK and NDK.  These dependencies will be removed
+  eventually, but for now you need to install both.  Follow the
+  instructions at
+
+    http://developer.android.com/sdk/installing.html
+    http://developer.android.com/sdk/ndk/index.html
+
+  From here on, this document assumes you installed the SDK to the
+  path $ANDROID_SDK and the NDK to the path $ANDROID_NDK.
 
  Quick start
 -------------
@@ -91,9 +94,17 @@ from upstream again, just re-run |make sync|.
 
 We're using these currently for reasons not worth knowing.
 
+# for android backend
+
 $ cat > local.mk << EOF
 ANDROID_SDK="$ANDROID_SDK"
 ANDROID_NDK="$ANDROID_NDK"
+EOF
+
+# for b2g backend
+
+$ cat > local.mk << EOF
+WIDGET_BACKEND=b2g
 EOF
 
  == Configure repo for your device ==
@@ -198,8 +209,27 @@ If all goes well, you'll see the phone reboot into the bootloader,
 then your host machine will report the progress of files it uploads
 and installs on the device.  After finishing, the device will reboot.
 
-After flashing, you may need to run
-adb shell chmod -R 777 /data/system
+# temporary hack you may need on the android backend
+
+$ adb shell chmod -R 777 /data/system
+
+# temporary hack you may need on the b2g backend
+
+$ adb shell chmod 755 /system/b2g/b2g && \
+  adb shell "cat /system/b2g/libmozutils.so > /system/lib/libmozutils.so"
+
+ == Kernel flashing ==
+
+The kernel should also be flashed when using the B2G backend on a SII.
+It only needs to be done once when switching to the B2G backend.
+This will overwrite ClockworkMod.
+
+GET A DOWNLOAD MODE JIG BEFORE ATTEMPTING THIS!
+The flashing process is known to fail.
+
+$ make kernel
+$ adb reboot download
+$ heimdall flash --kernel boot/kernel-android-galaxy-s2/arch/arm/boot/zImage
 
  == Install Gaia ==
 

config/gecko-prof-b2g
@@ -1,0 +1,21 @@
+mk_add_options MOZ_OBJDIR=@TOPSRCDIR@/objdir-prof-android
+
+# XXX our build system doesn't seem to respect toplevel -j ...
+# hard-coding this sucks
+mk_add_options MOZ_MAKE_FLAGS="-s -j16"
+
+# XXX need a plan for toolchain and multi-dev-platform support
+topdir="`pwd`/../.."
+gonk="$topdir/glue/gonk"
+sys=`uname -s | tr "[[:upper:]]" "[[:lower:]]"`
+
+ac_add_options --target=arm-linux-androidb2g
+ac_add_options --with-b2g-gonk="$gonk"
+ac_add_options --with-endian=little
+
+ac_add_options --enable-application=b2g
+ac_add_options --disable-elf-hack
+
+ac_add_options --enable-debug-symbols
+ac_add_options --enable-profiling
+ac_add_options --with-ccache

commit 6e90f5644ff7cdb100a7126d28250ee63840d236

config-qemu depends on config-gecko-android instead of config-gecko-gonk.
Fix the bug introduced by commit 3d12fe0 which renames
config-gecko-gonk to config-gecko-android, but misses this dependency.

Makefile
@@ -125,7 +125,7 @@ nexuss4g-postconfig:
 	$(call GONK_CMD,make signapk && vendor/samsung/crespo4g/reassemble-apks.sh)
 
 .PHONY: config-qemu
-config-qemu: config-gecko-gonk
+config-qemu: config-gecko-android
 	@echo "KERNEL = qemu" > .config.mk && \
 	echo "GONK = generic" >> .config.mk && \
 	echo "GONK_TARGET = generic-eng" >> .config.mk && \